0af8bf7c71c5c13aacae098ec29bb2669464ce8f
Ahmad Faqih Abdillah <19241870@bsi.ac.id>
2026-01-14 14:39:41 +0700

    Memperbarui Model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\ManajemenSekolah;
 
 class User extends Authenticatable
 {
@@ -21,7 +22,13 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
+
+    public function sekolah()
+    {
+        return $this->hasOne(ManajemenSekolah::class, 'user_id');
+    }
 
     /**
      * The attributes that should be hidden for serialization.
@@ -40,5 +47,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
 }
